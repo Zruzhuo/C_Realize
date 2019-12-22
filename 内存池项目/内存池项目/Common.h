bf4ecad43f4be343d70c113ebc58e20926c146bf
Zruzhuo <1197736628@qq.com>
2019-12-22 21:17:00 +0800

    内存池项目->完善

--- 内存池项目/内存池项目/Common.h
+++ 内存池项目/内存池项目/Common.h
@@ -1,15 +1,54 @@
 #pragma once
 
-#include"Common.h"
+#include<iostream>
+using namespace std;
 
-class ThreadCache {
+const size_t MAX_SIZE = 64 * 1024;
+const size_t N_FREE_LIST = MAX_SIZE / 8;
+
+inline void*& NextObj(void* obj)
+{
+	return *((void**)obj);
+}
+
+class FreeList {
 public:
-	//申请内存和释放内存
-	void* Allocte(size_t size);
-	void Deallocte(void* ptr, size_t size);
-
-	//从中心缓存获取对象
-	void* FetchFromCentralCache(size_t index);
+	void Push(void* obj) //头插
+	{
+		NextObj(obj) = _freelist;
+		_freelist = obj;
+	}
+	void PushRange(void* head, void* tail)
+	{
+		NextObj(tail) = _freelist;
+		_freelist = head;
+	}
+	void* Pop() //头删
+	{
+		void* obj = _freelist;
+		_freelist = NextObj(obj);
+		return obj;
+	}
+	bool Empty()
+	{
+		return _freelist == nullptr;
+	}
 private:
-	FreeList _freeLists[NFREE_LIST];
+	void* _freelist = nullptr;
+};
+
+class SizeClass
+{
+public:
+	static size_t ListIndex(size_t size)
+	{
+		if (size % 8 == 0)
+		{
+			return size / 8 - 1;
+		}
+		else
+		{
+			return size / 8;
+		}
+	}
 };
